458dfee9d1a2a0d3b609ae6227fec3f6e1f4ffe5
Victor Rentea <victorrentea@gmail.com>
2020-09-16 11:16:24 +0300

    gata curatarile banale. incepe treaba serioasa

--- src/refactoring/videostore/Customer.php
+++ src/refactoring/videostore/Customer.php
@@ -1,40 +1,30 @@
 <?php
+
 namespace victor\refactoring\videostore;
 
 class Customer
 {
-    public function __construct(string $name) {
+    private string $name;
+
+    /** @var Rental[] */
+    private array $rentals = [];
+
+    public function __construct(string $name)
+    {
         $this->name = $name;
     }
 
-    public function addRental (Rental $rental) {
+    public function addRental(Rental $rental): void
+    {
         $this->rentals[] = $rental;
     }
 
-
-    /**
-     * @return string
-     */
-    public function getName () {
-        return $this->name;
-    }
-
-
-    /**
-     * @param $name string
-     */
-    public function setName ($name) {
-        $this->name = $name;
-    }
-
-    /**
-     * @return string
-     */
-    public function statement () {
-        $totalAmount 			= 0;
-        $frequentRenterPoints 	= 0;
-        $rentals 				= $this->rentals;
-        $result 			= 'Rental Record for ' . $this->getName() . "\n";
+    public function statement(): string
+    {
+        $totalAmount = 0;
+        $frequentRenterPoints = 0;
+        $rentals = $this->rentals;
+        $result = 'Rental Record for ' . $this->getName() . "\n";
 
         foreach ($rentals as $each) {
             $thisAmount = 0;
@@ -52,7 +42,7 @@ class Customer
                 case Movie::TYPE_CHILDREN:
                     $thisAmount += 1.5;
                     if ($each->getDaysRented() > 3)
-                        $thisAmount += ($each->getDaysRented () - 3) * 1.5;
+                        $thisAmount += ($each->getDaysRented() - 3) * 1.5;
                     break;
             }
 
@@ -78,10 +68,9 @@ class Customer
         return $result;
     }
 
+    public function getName(): string
+    {
+        return $this->name;
+    }
 
-    private $name;
-    /**
-     * @var Rental[]
-     */
-    private $rentals = array();
 }
