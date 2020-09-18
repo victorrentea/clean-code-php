58fa23b3cbe7c26473bf0a7aea1eb3d0564ea812
Victor Rentea <victorrentea@gmail.com>
2020-09-18 11:07:45 +0300

    flatten the BF function

--- src/clean/BouleanParameters.php
+++ src/clean/BouleanParameters.php
@@ -19,20 +19,17 @@ $boule->bigUglyMethod987(1, 2);
 class BouleanParameters
 {
 
-	function bigUglyMethod987(int $a, int $b) {
+    function bigUglyMethod987(int $a, int $b)
+    {
         echo "Extra logic before the method here";
         $this->bigUglyMethod();
     }
-	function bigUglyMethod(int $a, int $b) {
-        $this->beforeLogic();
-        $this->afterLogic();
-    }
-	function bigUglyMethod323(int $a, int $b) {
-        $this->beforeLogic();
-        echo "My custom logic here, only when I call the functin\n";
-        $this->afterLogic();
-    }
 
+    function bigUglyMethod(int $a, int $b)
+    {
+        $this->beforeLogic();
+        $this->afterLogic();
+    }
 
     private function beforeLogic(): void
     {
@@ -48,35 +45,51 @@ class BouleanParameters
         echo "More Complex Logic\n";
     }
 
+    function bigUglyMethod323(int $a, int $b)
+    {
+        $this->beforeLogic();
+        echo "My custom logic here, only when I call the functin\n";
+        $this->afterLogic();
+    }
 
 
+    // ============== "BOSS" LEVEL: A lot harder to break down =================
 
-
-
-
-	// ============== "BOSS" LEVEL: A lot harder to break down =================
-
-	function bossLevel(bool $stuff, bool $fluff, array $tasks) {
+    function bossLevelStuffFluff(array $tasks)
+    {
+        echo "Logic1\n";
+        echo "Logic2\n";
+        echo "Logic3\n";
         $i = 0;
-		$j = 1;
-		echo "Logic1\n";
-		if ($stuff) {
-            echo "Logic2\n";
-            if ($fluff) {
-                echo "Logic3\n";
-                foreach ($tasks as $task) {
-                    $i++;
-                    echo "Logic4 " . $task . "\n";
-                    // TODO HERE, when call this method, I want MY own custom code to run here
-                    echo "Logic5 " . $i . "\n";
-                }
-				echo "Logic6 " . ($j++) . "\n";
-			}
-		}
-		echo "Logic7\n";
-	}
+        foreach ($tasks as $task) {
+            echo "Logic4 " . $task . "\n";
+
+            // TODO HERE, when call this method, I want MY own custom code to run here
+
+            echo "My Logic, that should run when I call this function\n";
+            // $this->abstractStep(); // template method
+            // $this->missingStep->call(); // composition of the behavior
+            // $missingStep->call(); // passing an object to do stuff, not just the boolean
 
 
+            $i++;
+            echo "Logic5 " . $i . "\n";
+        }
+        echo "Logic6 " . 1 . "\n";
+        echo "Logic7\n";
+    }
 
+    function bossLevelStuffNoFluff()
+    {
+        echo "Logic1\n";
+        echo "Logic2\n";
+        echo "Logic7\n";
+    }
+
+    function bossLevelNoStuff()
+    {
+        echo "Logic1\n";
+        echo "Logic7\n";
+    }
 
 }
