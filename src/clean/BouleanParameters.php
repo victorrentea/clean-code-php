6e2f8e079a00dc51529723e960bd8a95fc2d424c
Victor Rentea <victorrentea@gmail.com>
2020-11-19 12:07:57 +0200

    Cum faci ceva in plus fata de ce era facut deja intr-o functie

--- src/clean/BouleanParameters.php
+++ src/clean/BouleanParameters.php
@@ -4,22 +4,36 @@
 namespace victor\refactoring;
 
 $boule = new BouleanParameters();
-$boule->bigUglyMethod(1, 2);
-$boule->bigUglyMethod(1, 2);
-$boule->bigUglyMethod(1, 2);
-$boule->bigUglyMethod(1, 2);
-$boule->bigUglyMethod(1, 2);
+$boule->bigUglyMethod(1, 2, false);
+$boule->bigUglyMethod(1, 2, false);
+$boule->bigUglyMethod(1, 2, false);
+$boule->bigUglyMethod(1, 2, false);
+$boule->bigUglyMethod(1, 2, false);
 
 // TODO From my use-case, I call it too, to do more within:
-$boule->bigUglyMethod(1, 2);
+$boule->bigUglyMethodCR322(1, 2);
+
+$boule->bigUglyMethod(1, 2, true);
 
 class BouleanParameters
 {
 
-	function bigUglyMethod(int $a, int $b) {
+    function bigUglyMethodCR322(int $a, int $b)
+    {
+        echo "Inainte al meu $a";
+        $this->bigUglyMethod($a, $b, false);
+        echo "Dupa al meu $a";
+    }
+
+	function bigUglyMethod(int $a, int $b, bool $cr456) {
+
         echo "Complex Logic with $a\n";
         echo "Complex Logic $b\n";
         echo "Complex Logic $a\n";
+
+        if ($cr456) {
+            $this->bigUglyMethod($a, $b, $cr456);
+        }
 
         echo "More Complex Logic $a\n";
         echo "More Complex Logic $a\n";
