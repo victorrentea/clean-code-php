b5eba82a14da5e04827a8c435ea302e45cef2827
Victor Rentea <victorrentea@gmail.com>
2020-11-19 16:55:35 +0200

    Mi-am bagat piciorul in framework. Am facut NEW, spre disperarea lui!

--- src/clean/ManyParamsOOP.php
+++ src/clean/ManyParamsOOP.php
@@ -4,7 +4,7 @@
 namespace victor\clean;
 
 
-(new ManyParamsOOP(new Validator(new OtherDependency())))->bizLogic([
+(new ManyParamsOOP())->bizLogic([
     'a'=>'a',
     'b'=>1,
     's'=>'s',
@@ -20,11 +20,12 @@ namespace victor\clean;
 
 class ManyParamsOOP
 {
-    private $validator;
 
-    public function __construct(Validator $validator)
+    private OtherDependency $dep;
+
+    public function __construct(OtherDependency $dep)
     {
-        $this->validator = $validator;
+        $this->dep = $dep;
     }
 
     public function bizLogic(array $x)
@@ -34,16 +35,18 @@ class ManyParamsOOP
         $sellerId = 1;
         // > de 4/5 din functii au acelasi param in semnatura -===> setXsellerId()
 
+        $validator = new Validator($this->dep, $sellerId);
+
 //        $this->validator->setSellerId($sellerId);
 
         // 1 poti sa uiti s-o faci. starea lui validator poate sa fie incompleta. ==> bug runtime
         // 2 datorita DepInje-> 1 instanta de Validator / appp => sellerId ramane setat si poate sa 'curga': alte invocari ale validatorului in alte parti,
                 // ar putea sa foloseasca acelasi sellerId din greseala
-        $errors = array_merge($errors, $this->validator->m1($x['a'], $x['b']));
-        $errors = array_merge($errors, $this->validator->m2($x['a'], $x['s'], $x['c']));
-        $errors = array_merge($errors, $this->validator->m3($x['a'], $x['fileName'], $x['versionId'], $x['reference']));
-        $errors = array_merge($errors, $this->validator->m4($x['a'], $x['listId'], $x['recordId'], $x['g']));
-        $errors = array_merge($errors, $this->validator->m5($x['b']));
+        $errors = array_merge($errors, $validator->m1($x['a'], $x['b']));
+        $errors = array_merge($errors, $validator->m2($x['a'], $x['s'], $x['c']));
+        $errors = array_merge($errors, $validator->m3($x['a'], $x['fileName'], $x['versionId'], $x['reference']));
+        $errors = array_merge($errors, $validator->m4($x['a'], $x['listId'], $x['recordId'], $x['g']));
+        $errors = array_merge($errors, $validator->m5($x['b']));
         if (!empty($errors)) {
             throw new \Exception($errors);
         }
