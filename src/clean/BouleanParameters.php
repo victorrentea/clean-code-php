e7702cf250e3537731125a61cfa62a472d933dae
Victor Rentea <victorrentea@gmail.com>
2024-01-11 12:58:00 +0200

    split loop

--- src/clean/BouleanParameters.php
+++ src/clean/BouleanParameters.php
@@ -1,7 +1,13 @@
 <?php
 
-
 namespace victor\refactoring;
+
+$a = [1, 2];
+testDeModifArr($a);
+function testDeModifArr(array $arr)
+{
+    $arr[] = 3;
+}
 
 
 $boule = new BouleanParameters();
@@ -17,12 +23,14 @@ $boule->bigUglyMethodDocker(1, 2);
 class BouleanParameters
 {
 
-	function bigUglyMethod(int $ip, int $dnsNameCount) {
+    function bigUglyMethod(int $ip, int $dnsNameCount)
+    {
         $money = $this->initPyWorker($ip, $dnsNameCount);
         $this->audit($ip, $dnsNameCount, $money);
     }
 
-	function bigUglyMethodDocker(int $ip, int $dnsNameCount) {
+    function bigUglyMethodDocker(int $ip, int $dnsNameCount)
+    {
         $money = $this->initPyWorker($ip, $dnsNameCount);
 
         echo "Ceva doar pentru CR323";
@@ -43,40 +51,78 @@ class BouleanParameters
     }
 
 
+    // ============== "BOSS" LEVEL: A lot harder to break down =================
+    function bossLevel(array $tasks)
+    {
+        $i = $this->ini($tasks);
+        // TODO HERE, when call this method, I want MY own custom code to run here
 
-	// ============== "BOSS" LEVEL: A lot harder to break down =================
+        $this->disconnect($tasks);
+    }
+    function bossLevelDocker(array $tasks)
+    {
+        $i = $this->ini($tasks);
 
-	function bossLevel(bool $stuff, bool $fluff, array $tasks) {
-        $i = 0;
-		$j = 1;
-		echo "Logic1\n";
-		if ($stuff) {
-            echo "Logic2\n";
-            if ($fluff) {
-                echo "Logic3\n";
-                foreach ($tasks as $task) {
-                    $i++;
-                    echo "Logic4 " . $task . "\n";
-                    // TODO HERE, when call this method, I want MY own custom code to run here
-                    echo "Logic5 " . $i . "\n";
-                }
-				echo "Logic6 " . ($j++) . "\n";
-			}
-		}
-		echo "Logic7\n";
-	}
+        foreach ($tasks as $task) {
+            echo "Stuf";
+        }
+        // TODO HERE, when call this method, I want MY own custom code to run here
+
+        $this->disconnect($tasks);
+    }
 
     /**
-     * @param int $ip
+     * @param string[] $phpECumE
+     * @param int $ip the ip to scan for vulnerability. asa nu
      * @param int $dnsNameCount
+     * @param Array<string> $phpECumE
      * @return int
      */
-    public function initPyWorker(int $ip, int $dnsNameCount): int
+    public function initPyWorker(int $ip, int $dnsNameCount, array $phpECumE): int
     {
         echo "Complex Logic with $ip\n";
         echo "Complex Logic $dnsNameCount\n";
         echo "Complex Logic $ip\n";
         $money = 1;
         return $money;
+    }
+
+    /**
+     * @param array $tasks
+     * @return int|string
+     */
+    public function ini(array $tasks)
+    {
+        echo "Logic1\n";
+
+        echo "Logic2\n";
+        echo "Logic3\n";
+
+        foreach ($tasks as $i => $task) {
+            echo "Logic4 " . $task . "\n";
+//            connectToWorker($task)
+        }
+        return $i;
+    }
+
+    /**
+     * @param array $tasks
+     * @return void
+     */
+    public function disconnect(array $tasks): void
+    {
+        foreach ($tasks as $i => $task) {
+            echo "Logic5 " . ($i + 1) . "\n";
+//            disconnectWorker($task);
+        }
+
+        $taskCount = count($tasks);
+        echo "Logic6 $taskCount\n";
+        echo "Logic7\n";
+    }
+
+    private function innocent(array &$tasks)
+    {
+        $tasks[] = "#sieu";
     }
 }
