38d0ad964bc80e326fd29dd4f18693c393a34f69
Victor Rentea <victorrentea@gmail.com>
2025-05-05 15:45:29 +0300

    split the for (SHOCK!) no performance impact recorded by victor in 300 projects.

--- src/refactoring/SplitLoop.php
+++ src/refactoring/SplitLoop.php
@@ -6,17 +6,29 @@ namespace victor\refactoring;
 class SplitLoop {
     /** @param Employee[] $employees */
     static function computeStats(array $employees) {
-        $averageAge = 0;
+        $totalAge = 0;
+        foreach ($employees as $e) {
+            $totalAge += $e->age;
+        }
+
         $totalSalary = 0;
         foreach ($employees as $e) {
-            $averageAge += $e->age;
             $totalSalary += $e->salary;
         }
-        $averageAge = $averageAge / sizeof($employees);
+        // repeating a for loop will NOT produce performance issues.
+        // a) collection is small => overhead = minimal
+        // b) collection is large => Q: where did that collection come from?
+        //  > API call (Fb) ~50-200ms...
+        //  > SQL (SELECT)~1-10ms
+        //  > Redis (<1ms response)
+        // if you bring over the network,  the overehad of that NETWORK
+        // CALL WILL GREATLY OUTWEIGH the potential loss of performance
+        $averageAge = $totalAge / sizeof($employees);
         $averageSalary = $totalSalary / sizeof($employees);
         echo "avg age = $averageAge\navg sal = $averageSalary\n";
     }
 }
+//        $averageAge = array_average($employees, fn($emp)=>$emp->salary) // high cost
 
 SplitLoop::computeStats([new Employee(24, 1500), new Employee(30, 2500)]);
 
