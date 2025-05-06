c1fa74d72ea9b5aad0d1b8e7bbd195b82a2ec905
Victor Rentea <victorrentea@gmail.com>
2025-05-06 15:46:13 +0300

    done

--- src/refactoring/videostore/Customer.php
+++ src/refactoring/videostore/Customer.php
@@ -46,22 +46,39 @@ class Customer
 
     private function calculateRentalSummary(): array
     {
-        return array_reduce(
-            $this->rentals,
-            function (array $summary, Rental $rental) {
-                $summary['totalAmount'] += $rental->getCharge();
-                $summary['frequentRenterPoints'] += $rental->getFrequentRenterPoints();
-                return $summary;
-            },
-            ['totalAmount' => 0.0, 'frequentRenterPoints' => 0]
-        );
+//        return array_reduce(
+//            $this->rentals,
+//            function (array $summary, Rental $rental) {
+//                $summary['totalAmount'] += $rental->getCharge();
+//                $summary['frequentRenterPoints'] += $rental->getFrequentRenterPoints();
+//                return $summary;
+//            },
+//            ['totalAmount' => 0.0, 'frequentRenterPoints' => 0]
+//        );
+
+        $totalAmount = 0.0;
+        foreach ($this->rentals as $rental) {
+            $totalAmount += $rental->getCharge();
+        }
+
+        $frequentRenterPoints = 0;
+        foreach ($this->rentals as $rental) {
+            $frequentRenterPoints += $rental->getFrequentRenterPoints();
+        }
+        return ['totalAmount' => $totalAmount, 'frequentRenterPoints' => $frequentRenterPoints];
     }
 
+    /**
+     * @param Rental[] $rentals
+     */
     private function formatRentalLines(array $rentals): string
     {
-        return implode('', array_map(
-            fn(Rental $rental) => "\t{$rental->getMovie()->getTitle()}\t" . $rental->getCharge() . "\n",
-            $rentals
-        ));
+        $lines = array_map(fn(Rental $rental) =>$this->formatLine($rental),$rentals);
+        return implode('', $lines);
+    }
+
+    private function formatLine(Rental $rental): string
+    {
+        return "\t{$rental->getMovie()->getTitle()}\t" . $rental->getCharge() . "\n";
     }
 }
