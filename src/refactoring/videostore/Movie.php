1391eb69e6c702d1610ad986ae457abc4ef31ade
Prathamesh Pandit <prathamesh.pandit@nice.com>
2025-05-06 15:35:18 +0530

    Refactor Movie and Rental class #2 (Prathamesh)

--- src/refactoring/videostore/Movie.php
+++ src/refactoring/videostore/Movie.php
@@ -6,18 +6,15 @@ class Movie
     const NEW_RELEASE = "NEW_RELEASE";
     const REGULAR = "REGULAR";
     const CHILDREN = "CHILDREN";
-    private string $title;
-    private string $priceCode;
+
 
     /**
      * Movie constructor.
      * @param string $title
      * @param string $priceCode
      */
-    public function __construct(string $title, string $priceCode)
+    public function __construct(private string $title, private string $priceCode)
     {
-        $this->title = $title;
-        $this->priceCode = $priceCode;
     }
 
     /**
@@ -51,5 +48,4 @@ class Movie
     {
         $this->priceCode = $priceCode;
     }
-
 }
