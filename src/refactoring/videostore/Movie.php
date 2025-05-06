418995fe91650761abbbd25aeebabafd7e924c80
neha-dhawale <neha.dhawale@nice.com>
2025-05-06 15:25:03 +0530

    Refactor Movie class #1 (Neha)

--- src/refactoring/videostore/Movie.php
+++ src/refactoring/videostore/Movie.php
@@ -5,51 +5,49 @@ class Movie
 {
     const NEW_RELEASE = "NEW_RELEASE";
     const REGULAR = "REGULAR";
-    const CHILDRENS = "CHILDRENS";
-
-    private $title;
-
-    private $priceCode;
+    const CHILDREN = "CHILDREN";
+    private string $title;
+    private string $priceCode;
 
     /**
      * Movie constructor.
      * @param string $title
      * @param string $priceCode
      */
-    public function __construct($title, $priceCode)
+    public function __construct(string $title, string $priceCode)
     {
         $this->title = $title;
         $this->priceCode = $priceCode;
     }
 
     /**
-     * @return mixed
+     * @return string
      */
-    public function getTitle()
+    public function getTitle() : string
     {
         return $this->title;
     }
 
     /**
-     * @param mixed $title
+     * @param string $title
      */
-    public function setTitle($title)
+    public function setTitle(string $title) : void
     {
         $this->title = $title;
     }
 
     /**
-     * @return mixed
+     * @return string
      */
-    public function getPriceCode()
+    public function getPriceCode() : string
     {
         return $this->priceCode;
     }
 
     /**
-     * @param mixed $priceCode
+     * @param string $priceCode
      */
-    public function setPriceCode($priceCode)
+    public function setPriceCode(string $priceCode) : void
     {
         $this->priceCode = $priceCode;
     }
