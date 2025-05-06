7d4599c387b7512dcb9bee7c9e28309304a10c8d
Hinal Ka.Patel <hinalben.kapatel@nice.com>
2025-05-06 16:17:59 +0530

    WIP :Refactor Customer Class #4 (Hinal)

--- src/refactoring/videostore/Customer.php
+++ src/refactoring/videostore/Customer.php
@@ -7,6 +7,8 @@ class Customer
      * @var Rental[] $rentals
      */
     private array $rentals = [];
+    const MAX_RENTAL_REGULAR = 2;
+    const MAX_RENTAL_CHILDREN = 3;
 
     public function __construct(private string $name) {
     }
@@ -33,38 +35,36 @@ class Customer
         $rentals 				= $this->rentals;
         $result 			= 'Rental Record for ' . $this->getName() . "\n";
 
-        foreach ($rentals as $each) {
-            $thisAmount = 0;
+        foreach ($rentals as $rental) {
+            $rentalAmount = 0;
 
-            // determines the amount for each line
-            switch ($each->getMovie()->getPriceCode()) {
+            // determines the amount for rental line
+            $daysRented = $rental->getDaysRented();
+            switch ($rental->getMovie()->getPriceCode()) {
                 case Movie::REGULAR:
-                    $thisAmount += 2;
-                    if ($each->getDaysRented() > 2)
-                        $thisAmount += ($each->getDaysRented() - 2) * 1.5;
+                    $rentalAmount += 2;
+                    if ($daysRented > self::MAX_RENTAL_REGULAR)
+                        $rentalAmount += ($daysRented - self::MAX_RENTAL_REGULAR) * 1.5;
                     break;
                 case Movie::NEW_RELEASE:
-                    $thisAmount += $each->getDaysRented() * 3;
+                    $rentalAmount += $daysRented * 3;
+                    if($daysRented > 1)
+                        $frequentRenterPoints++;
                     break;
                 case Movie::CHILDREN:
-                    $thisAmount += 1.5;
-                    if ($each->getDaysRented() > 3)
-                        $thisAmount += ($each->getDaysRented () - 3) * 1.5;
+                    $rentalAmount += 1.5;
+                    if ($daysRented > self::MAX_RENTAL_CHILDREN)
+                        $rentalAmount += ($daysRented - self::MAX_RENTAL_CHILDREN) * 1.5;
                     break;
             }
 
             // add frequent renter points
             $frequentRenterPoints++;
 
-            // add bonus for a two day new release rental
-            if ($each->getMovie()->getPriceCode() == Movie::NEW_RELEASE
-                && $each->getDaysRented() > 1)
-                $frequentRenterPoints++;
-
             // add footer lines
-            $result .= "\t" . $each->getMovie()->getTitle() . "\t"
-                . $thisAmount . "\n";
-            $totalAmount += $thisAmount;
+            $result .= "\t" . $rental->getMovie()->getTitle() . "\t"
+                . $rentalAmount . "\n";
+            $totalAmount += $rentalAmount;
 
         }
 
